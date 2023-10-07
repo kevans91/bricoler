dcdde4ba112f72048810994c50aa8f9a6a46ba21
Mark Johnston <markjdb@gmail.com>
2023-10-07 11:48:33 -0400

    Update tests

--- tests/_env.sh
+++ tests/_env.sh
@@ -3,6 +3,7 @@
 # Common definitions for all shell-based tests.
 
 export PATH=$(atf_get_srcdir)/..:${PATH}
-export LUA_PATH_5_4=";;${LUA_PATH_5_4};$(atf_get_srcdir)/../?.lua"
+export LUA_PATH_5_4=";;$(atf_get_srcdir)/../?.lua"
+export LUA_CPATH_5_4=";;$(atf_get_srcdir)/../lib/freebsd/?.so"
 export BRICOLER_TASKDIR=$(atf_get_srcdir)/tasks
 export BRICOLER_WORKDIR=$(pwd)/bricoler
