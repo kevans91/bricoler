edfe744878ea31362a39014f02def87763448472
Mark Johnston <markjdb@gmail.com>
2023-10-28 19:39:53 -0400

    Update libpath for tests

--- tests/_env.sh
+++ tests/_env.sh
@@ -4,6 +4,6 @@
 
 export PATH=$(atf_get_srcdir)/..:${PATH}
 export LUA_PATH_5_4=";;$(atf_get_srcdir)/../?.lua"
-export LUA_CPATH_5_4=";;$(atf_get_srcdir)/../lib/freebsd/?.so"
+export LUA_CPATH_5_4=";;$(atf_get_srcdir)/../lib/freebsd/sys/?/?.so"
 export BRICOLER_TASKDIR=$(atf_get_srcdir)/tasks
 export BRICOLER_WORKDIR=$(pwd)/bricoler
